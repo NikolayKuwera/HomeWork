189c1b509a07b7cf769b65d97486c9c9edfe91ad
NikolayKuwera <aragorn74@mail.ru>
2024-11-07 21:08:50 +0300

    27/10/24

--- module_1_3.py
+++ module_1_3.py
@@ -3,9 +3,9 @@ from os import times
 name = 'Nikolay'
 print('Name:', name)
 age = 50
-print('Age:',age)
+print('Age:', age)
 age = age + 1
-print('New age:',age)
+print('New age:', age)
 is_student = True
 print('Is student:', is_student)
 
