47a0be1addfee814b034648cf3405906ac6c9869
NikolayKuwera <aragorn74@mail.ru>
2024-11-12 19:34:02 +0300

    27/10/24 Initial

--- main1.py
+++ main1.py
@@ -1,19 +1,19 @@
-# n = list(range(1, 21))
-# b = n.copy()
-# m = []
-# for i in n:
-#     if i % 2 == 0:
-#         m.append(i)
-#         n.remove(i)
-# else:
-#     print(n)
-#     print(m)
-# print(b)
+n = list(range(1, 21))
+b = n.copy()
+m = []
+for i in n:
+    if i % 2 == 0:
+        m.append(i)
+        n.remove(i)
+else:
+    print(n)
+    print(m)
+print(b)
 
-from time import sleep
-
-a = 5
-print(a)
-print('я тут')
-sleep(4)
-print('Фух 4 секунды прошло')
+# from time import sleep
+#
+# a = 5
+# print(a)
+# print('я тут')
+# sleep(4)
+# print('Фух 4 секунды прошло')
