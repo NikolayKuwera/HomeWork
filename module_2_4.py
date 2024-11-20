26e3c26c049c879763a4bba43d1d534cc8d7d62d
NikolayKuwera <aragorn74@mail.ru>
2024-11-20 18:34:39 +0300

    27/10/24 Initial

--- module_2_4.py
+++ module_2_4.py
@@ -2,16 +2,13 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 for number in numbers:
-    if number == 1:
-        continue
-    is_prime = True  # Предполагаем, что число простое
-    for i in range(2, number):
-        if number % i == 0:
+    is_prime = True
+    for devizor in range(2, number):
+        if number % devizor == 0:
+            not_primes.append(number)
             is_prime = False
             break
-    if is_prime:  # Если число простое, добавляем его в список primes, иначе - в not_primes
+    if is_prime == True and number != 1:
         primes.append(number)
-    else:
-        not_primes.append(number)
 print("Простые числа:", primes)
 print("Не простые числа:", not_primes)
