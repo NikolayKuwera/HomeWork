fbeb691c02e08123162bbf43300d31ecd92c52b5
NikolayKuwera <aragorn74@mail.ru>
2024-11-20 19:21:34 +0300

    Дополнительное практическое задание по модулю: "Основные операторы"
    Цель: Применить знания полученные в модуле, решив задачу повышенного уровня сложности
    
    Задание "Слишком древний шифр":
    
    Вы отправились в путешествие на необитаемый остров и конечно же в очередной вылазке в джунгли вы попали в ловушку местному племени (да-да, классика "Индиана Джонса").
    
    К вашему удивлению, в племени были неплохие математики и по совместительству фантазёры.
    
    Вы поняли это, когда после долгих блужданий перед вами появились ворота (выход из ловушки) с двумя каменными вставками для чисел.
    
    В первом поле камни с числом менялись постоянно (от 3 до 20) случайным образом, а второе было всегда пустым.
    
    К вашему счастью рядом с менее успешными и уже неговорящими путешественниками находился папирус, где были написаны правила для решения этого "ребуса". (Как жаль, что они поняли это так поздно :( ).
    
    Во вторую вставку нужно было написать те пары чисел друг за другом, чтобы число из первой вставки было кратно(делилось без остатка) сумме их значений.
    
    Пример кратности(деления без остатка):
    
    1 + 2 = 3 (сумма пары)
    
    9 / 3 = 3 (ровно 3 без остатка)
    
    9 кратно 3 (9 делится на 3 без остатка)
    
    Пример 1:
    
    9 - число из первой вставки
    
    1218273645 - нужный пароль (1 и 2, 1 и 8, 2 и 7, 3 и 6, 4 и 5 - пары; число 9 кратно сумме каждой пары)
    
    Пример 2:
    
    11 - число из первой вставки
    
    11029384756 - нужный пароль (1 и 10, 2 и 9, 3 и 8, 4 и 7, 5 и 6 - пары; число 11 кратно сумме каждой пары)

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,9 +1,7 @@
-import random
-
-elem = random.randint(3, 20)
-password = ''
-for x in range(1, elem):
-    for y in range(x + 1, elem):
-        if elem % (x + y) == 0:
-            password += f'{x}{y}'
-print(f'{elem} - {password}')
+for elem in range(3, 21):
+    password = f'Элемент:{elem} '
+    for x in range(1, elem):
+        for y in range(x + 1, elem):
+            if elem % (x + y) == 0:
+                password += f'{x}+{y} '
+    print(password)
