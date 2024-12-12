a889dc9664ea181aad943a1a9ca046d2339bbcde
NikolayKuwera <aragorn74@mail.ru>
2024-12-12 17:27:14 +0300

    26.11.2024

--- module_3/Same_num.py
+++ module_3/Same_num.py
@@ -1,7 +1,8 @@
 def single_root_words(root_word, *other_words):
     same_words = []
+    root_word = root_word.lower()
     for word in other_words:
-        if word.lower() in root_word.lower() or root_word.lower() in word.lower():
+        if word.lower() in root_word or root_word in word.lower():
             same_words.append(word)
     return same_words
 
